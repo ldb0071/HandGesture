4e2986b1ec780f57c4d291fd7986363a489b013c
ABDELLAH ZAKARIA SELLAM <76110908+ldb0071@users.noreply.github.com>
2022-10-15 15:13:04 +0100

    Update subtaractor.py

--- subtaractor.py
+++ subtaractor.py
@@ -1,9 +1,6 @@
 import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-
-# from keras.preprocessing import image
-# from keras.models import load_model
 import cv2
 import os
 import sys
@@ -15,10 +12,12 @@ from numpy.lib.function_base import append
 import multiprocessing_test as MPI
 from playsound import playsound
 
+
+
+#Values initialization 
 text=None
 end_1=0
 count=0
-
 CLASS_MAP = {0:'fist',
    1:'five',
    2:'none',
@@ -30,12 +29,14 @@ CLASS_MAP = {0:'fist',
 
 NUM_CLASSES = len(CLASS_MAP)
 
+#Maping the values to the classes
 def mapper(val):
     return CLASS_MAP[val]
 
+
+#hand gesture recognition list
 def list_com():
-    # now that we have the background, we can segment the hand.
-        
+    
         cv2.putText(frame_copy, "Index 0: Fist", (330, 355), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255),1)
         cv2.putText(frame_copy, "Index 1: Five", (330, 370), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255),1)
         cv2.putText(frame_copy, "Index 2: None", (330, 385), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255),1)
@@ -47,21 +48,22 @@ def list_com():
 
 
              
-
+#A function that calculates the accumulated average of the background
 def calc_accum_avg(frame, accumulated_weight):
+    #initializing the background
     global background
-    
+    #checking if the background is None
     if background is None:
         background = frame.astype("float")
-        ddd=frame
-       
-        cv2.imshow('backgrounder',ddd)
-        
+        frame_data=frame
+        # cv2.imshow('backgrounder',frame_data)
         return None
+    #computing weighted average, accumulate it and update the background
     cv2.accumulateWeighted(frame, background, accumulated_weight)
     
 
 
+#A function that reads the video feed and apply gray scale and gaussian blur then return the resulted frame
 def camera(cami):
     ret, frame = cami.read()
     frame = cv2.flip(frame, 1)
@@ -70,6 +72,8 @@ def camera(cami):
     gray = cv2.GaussianBlur(gray, (7, 7), 0)
     return frame,frame_copy,gray 
 
+
+# A function that segment the hand region and returns the thresholded image
 def segment(frame, threshold=20):
     global background
     diff = cv2.absdiff(background.astype("uint8"), frame)
@@ -83,31 +87,36 @@ def segment(frame, threshold=20):
         return None
     else:
         return thresholded
-
+    
+    
+# A function that communicates with the arduino and send the hand gesture to the raspberry pi using socket library 
 def comunication(msg) :          
-    s = socket.socket()         # Create a socket object
+    s = socket.socket()  # Create a socket object
     s.connect(("192.168.1.44",1234))
     if msg != None:
         ss=str(msg)
         s.sendall(ss.encode()) 
     s.close()
+    
+    
 
-
+#A function that feed the segmented hand to the model and predict the gesture
 def thres_display(img):
-    width=32
-    height=85
+    width=64
+    height=64
     dim=(width,height)
     resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    test_img=img_to_array(resized)
+    test_img=image.img_to_array(resized)
     test_img=np.expand_dims(test_img,axis=0)
     test_img /= 255
     result= newmod.predict(test_img)
-  
-    
     #print("%s: %.2f%%" % (newmod.metrics_names[1], result[1]*100))    
     val=[index for index,value in enumerate(result[0]) if value ==1]
     return val
 
+
+
+#A function apply the command based on the gesture
 def descion(k):
  if k >= 4  :
                             print('two seconds have been past')
@@ -115,6 +124,8 @@ def descion(k):
                             cam.release()
                             cv2.destroyAllWindows()
                             try:
+                                if res[0]==3:
+                                    comunication(res[0])
                                 if res[0]==3:
                                     comunication(res[0])    
                                 
@@ -136,7 +147,7 @@ def descion(k):
                                     #playsound('sound_mode_shut.mp3')
                                 if res[0]== 7: 
                                         pass
-                                    #playsound('turn on the fan.mp3')     
+                                       #playsound('turn on the fan.mp3')     
                                 num_frames = 0
                                 if num_frames==0:
                                     os.execv(sys.executable, ['python'] + sys.argv)           
@@ -144,15 +155,18 @@ def descion(k):
                                 num_frames = 0                         
  else:
                     k=0   
+
+
+
+
+
+
+
+
+#playsound('open_system.mp3')
 tes=MPI.main()
 tes
-#playsound('open_system.mp3')
-
-
-
-newmod=load_model('zack.h5')
-
-
+newmod=load_model('model_colab_1.h5')
 background = None
 accumulated_weight = 0.3
 zz=0
@@ -161,17 +175,25 @@ cam = cv2.VideoCapture(0)
 kl=0
 n=0
 num_frames = 0
-
 cam.set(3, 720)
 cam.set(4, 480)
 #playsound('background.mp3')
+
+
+
+
 while True:
+    
     frame,frame_copy,gray=camera(cam)
     if num_frames < 60:
+        
         calc_accum_avg(gray, accumulated_weight)
+        
         if num_frames <= 59:
+            
             cv2.putText(frame_copy, "GETTING BACKGROUND AVG", (200, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)       
-    else:  
+    else: 
+         
         list_com()
         hand = segment(gray)
         img=frame
@@ -180,10 +202,13 @@ while True:
         lmList, bbox = detector.findPosition(img, draw=False)
         global test
         test=detector.handType(img)  
+        
         if hand is not None:
             thresholded = hand
-            start = time.time() 
+            start = time.time()
+             
             try:
+                
                hans=detector.hands
                x_1,y_1,w_1,h_1=detector.roi_extractor(frame,hans)
                threshold=thresholded.copy()[y_1-20:y_1+h_1+10,x_1-20:x_1+w_1+10]
@@ -197,38 +222,50 @@ while True:
                kl=((end_1-start)/10**10)
                descion(kl)      
                cv2.imshow("Threshol Image", threshold)
+               
             except:
-                # cv2.destroyWindow('Threshol Image')
+                
+                cv2.destroyWindow('Threshol Image')
                 pass
+            
             try:
+                
               res=thres_display(threshold)
               x=mapper(res[0])
               cv2.putText(frame_copy, str(res[0])+':'+x, (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+              
             except:
+                
                 res=[]
             if len(res)==0:
+                
                cv2.putText(frame_copy,'None', (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
                res=[]
+               
             else: 
+                
                     pass
+                   
                
         if test =='Left':
             start=0           
             end_1=0
             kl=0  
-            #num_frames=40
+
+            
+            
 
     num_frames += 1
     cv2.imshow("Hand Gestures", frame_copy)
     k = cv2.waitKey(10) & 0xFF
     if k == ord('r'):
         num_frames=0
-
-    
-            
+          
     if k == ord('z'):
-       
         break
+
+
+
 
 cam.release()
 cv2.destroyAllWindows()
